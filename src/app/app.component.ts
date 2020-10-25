b2978d4491fc0a96ea9bab9039da31b25f72c27e
unknown <nadinegadjou@gmail.com>
2020-10-25 14:26:16 -0400

    added files

---
+++ src/app/app.component.ts
@@ -0,0 +1,122 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ModalDirective } from '../../node_modules/ngx-bootstrap';
+import { QuestionClass } from './question';
+import { ToastrService } from 'ngx-toastr';
+
+
+
+
+@Component({
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+	isQuestionCardShow: boolean = false;
+	totalAnswered: number = 0;
+	rightAnswer: number;
+	questionObj = new QuestionClass();
+	@ViewChild('submitModal') submitModal: ModalDirective;
+	@ViewChild('addQuestionModal') addQuestionModal : ModalDirective;
+	@ViewChild('answerModal') answerModal : ModalDirective;
+	@ViewChild('questionForm') questionForm: any;
+	@ViewChild('questionTest') questionTest : any;
+
+	constructor( private toastr: ToastrService) { }
+
+	answerArray = [];
+
+	allQuestions: any = [{
+		"id": 1,
+		"question":"Which HTML tag do we use to put the JavaScript?",
+		"a": "<javascript>",
+		"b": "<script>",
+		"c": "<js>",
+		"d": "None of the above",
+		"answer": "b"
+	},
+	{
+		"id": 2,
+		"question":"Which built-in method calls a function for each element in the array?",
+		"a": "while()",
+		"b": "loop",
+		"c": "forEach",
+		"d": "takeUntil",
+		"answer": "c"
+	},
+	{
+		"id": 3,
+		"question": "What is the difference between let and var?",
+		"a": "let has local scope",
+		"b":  "Both are same",
+		"c": "var is new data type",
+		"d": "let consumes more cpu and ram",
+		"answer": "a"
+	},
+	{
+		"id": 4,
+		"question": "Which of the following is NOT the method of an Array?",
+		"a": ".map()",
+		"b":  ".includes()",
+		"c": ".subscribe()",
+		"d":  ".flatMap()",
+		"answer": "c"
+	}
+	];
+
+	/**Method call on submit the test */
+	submitTest() {
+		this.rightAnswer = 0;
+		this.totalAnswered = 0;
+		for (let i = 0; i < this.allQuestions.length; i++) {
+			if ("selected" in this.allQuestions[i] && (this.allQuestions[i]["selected"] != null)) {
+				this.totalAnswered++;
+				if (this.allQuestions[i]["selected"] == this.allQuestions[i]["answer"]) {
+					this.rightAnswer++;
+				}
+			}
+
+		}
+		this.submitModal.show();
+
+	}
+
+	startQuiz() {
+		for (let i = 0; i < this.allQuestions.length; i++) {
+			if ("selected" in this.allQuestions[i]) {
+				delete this.allQuestions[i]["selected"];
+			}
+
+		}
+		this.questionTest.reset();
+		this.isQuestionCardShow = true;
+
+	}
+	HomePage() {
+		this.isQuestionCardShow = false;
+	}
+	addQuestion(){
+		this.addQuestionModal.show();
+	}
+	submitAddQuestion(){
+		let quesTemp = JSON.parse(JSON.stringify(this.questionObj));
+		quesTemp["id"] = this.allQuestions.length+1;
+		this.allQuestions.push(quesTemp);
+		this.questionForm.reset();
+		this.toastr.success("Question Added Successfully!!");
+		this.addQuestionModal.hide();
+
+	}
+	checkAnswers(){
+		this.submitModal.hide();
+		this.answerModal.show();
+	}
+
+	ngOnInit() {
+
+
+
+	}
+
+}
